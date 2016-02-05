4fb709ce5c0b9e62008da200d351dba1355a8e0f
DesertEagle PWN <deserteagle.pwn@gmail.com>
2016-02-04 19:06:35 -0500

    Preliminary Camera Controller
    Implemented a Pan-able Camera Controller Behavior and attached it to the
    scene's main camera.

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 priorMousePosition;
+    Vector3 mouseDeltaVector;
+
+    [SerializeField]
+    private float cameraSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        priorMousePosition = Input.mousePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            mouseDeltaVector = (priorMousePosition - Input.mousePosition); //store vector from old pos to new pos.
+            mouseDeltaVector.Normalize(); //Get the direction of the difference
+
+            Vector3 newPosition;
+            newPosition.x = gameObject.transform.position.x - mouseDeltaVector.x * Time.deltaTime * cameraSpeed;
+            newPosition.y = gameObject.transform.position.y;
+            newPosition.z = gameObject.transform.position.z - mouseDeltaVector.y * Time.deltaTime * cameraSpeed;
+
+            gameObject.transform.position = newPosition;
+        } //Middle mouse button down
+
+    }
+
+
+}
