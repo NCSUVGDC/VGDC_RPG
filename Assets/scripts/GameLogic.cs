678c51de335ba970b76500cb6381657526bdacfd
jegood <jegood@ncsu.edu>
2017-07-22 13:11:53 -0400

    Update GameLogic.cs
    Line fix.

--- Assets/scripts/GameLogic.cs
+++ Assets/scripts/GameLogic.cs
@@ -187,7 +187,7 @@ namespace VGDC_RPG
         {
             get
             {
-                return MatchInfo.PlayerInfos[CurrentPlayer].PlayerType.Equals(MatchInfo.PlayerType.Local)
+                return MatchInfo.PlayerInfos[CurrentPlayer].PlayerType.Equals(MatchInfo.PlayerType.Local);
             }
         }
 
